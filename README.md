df7e8b4c343ca49b55f67c2cba556e50477dd293
Andrew Tolentino <AndoyT2005@gmail.com>
2023-11-30 11:09:57 -0600

    Update files.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # snake-game
+# Credit: BroCode on youtube
--- snake-game.py
+++ snake-game.py
@@ -1,1 +1,45 @@
+from tkinter import *
+import random
 
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = 'green'
+FOOD_COLOR = 'red'
+BACKGROUND_COLOR = 'black'
+
+
+class Snake:
+    pass
+
+class Food:
+    pass
+
+def next_turn():
+    pass
+
+def change_direction():
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
+
+window = Tk()
+window.title("Python Game")
+window.resizable(False, False)
+
+score = 0
+direction = 'down'
+
+label = Label(window, text="Score:{}".format(score), font=('consolas', 40))
+label.pack()
+
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
+canvas.pack
+
+window.mainloop()
