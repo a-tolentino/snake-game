8f1035a4f554f3674674241c99a2bfbdd038fef4
Andrew Tolentino <AndoyT2005@gmail.com>
2023-12-14 15:39:28 -0600

    Update files.

--- snake-game.py
+++ snake-game.py
@@ -6,9 +6,9 @@ GAME_HEIGHT = 700
 SPEED = 50
 SPACE_SIZE = 50
 BODY_PARTS = 3
-SNAKE_COLOR = 'green'
-FOOD_COLOR = 'red'
-BACKGROUND_COLOR = 'black'
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
 
 
 class Snake:
@@ -20,7 +20,7 @@ class Food:
 def next_turn():
     pass
 
-def change_direction():
+def change_direction(new_direction):
     pass
 
 def check_collisions():
@@ -41,5 +41,17 @@ label.pack()
 
 canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
 canvas.pack
+
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (screen_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
 window.mainloop()
